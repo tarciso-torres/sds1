9e523169a8b12a7366789f0988796233643a695e
Tarciso Torres <tarcisoftorres@gmail.com>
2021-05-19 15:22:24 -0300

    chore: create RecordResrouce

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dspesquisa.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInserDTO;
+import com.devsuperior.dspesquisa.services.RecordService;
+
+@RestController
+@RequestMapping(value = "/records")
+public class RecordResource {
+	
+	@Autowired
+	private RecordService service;
+	
+	@PostMapping
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInserDTO dto) {
+		return ResponseEntity.ok().body(service.insert(dto));
+	}
+}
